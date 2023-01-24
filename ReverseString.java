02b76d2fcaa444c0334057c381572c6dae3b39d7
PAVANMK08 <123401832+PAVANMK08@users.noreply.github.com>
2023-01-24 16:54:56 +0000

    added a new program

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString{
+  public static void main(String[] args){
+    String str= "pavan";
+    String rev= "";
+    int len= str.length();
+    for(int i=len-1; i>=0; i--)
+       {
+         rev= rev+str.charAt(i);
+       }
+    System.out.println("Reverse String is: "+rev);
+  }
+}
