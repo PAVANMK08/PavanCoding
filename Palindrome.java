cf0ddc67b34b34119d07b9d23022a993950521be
PAVANMK08 <123401832+PAVANMK08@users.noreply.github.com>
2023-01-25 13:59:19 +0000

    updated a new program

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Palindrome{
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter a number");
+    int num= sc.nextInt();
+    int rev= 0;
+    int rem;
+    int temp;
+    temp=num;
+
+    while(temp!=0){
+      rem= temp%10;
+      rev= rev*10+rem;
+      temp/=10;
+    }
+
+    if(num==rev){
+      System.out.println(num+" is palindrome");
+    }
+    else{
+      System.out.println(num+" is not palindrome");
+    }
+  }
+}
